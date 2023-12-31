2b1444ca1a0402ae1ef1524be183cee3ed1cf0de
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2023-12-31 13:05:26 -0300

    TEste 2

--- src/main.js
+++ src/main.js
@@ -2,14 +2,15 @@ import { useNavigate } from "./lib/@prix.js";
 import Pages from "./page/index.js";
 import Components from "./Components/index.js";
 import Config from "./config.js";
-import api from"./api.js"
-
-const Data = await api()
+import api from "./api.js"
 
 
 
-
- useNavigate("/dashboard/", { Pages, Components, Data, Config});
+async function main() {
+    const Data = await api()
+    useNavigate("/dashboard/", { Pages, Components, Data, Config });
+}
+main()
 
 
 
